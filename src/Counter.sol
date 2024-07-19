8d0001d1e071138a92558b74a70251de919ae5d0
samooyo <samidarnaud@gmail.com>
2024-07-19 14:27:52 +0200

    perf: removed functions in libs

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract Counter {
     uint256 public number;
+    AggregatorV3Interface internal immutable _priceFeed;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
